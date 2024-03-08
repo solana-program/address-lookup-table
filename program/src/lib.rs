8376c001662d4cf011e95bdd18631ff1a94fdff7
Joe C <joe.caulfield@anza.xyz>
2024-03-08 10:16:17 -0600

    drop items moved in SDK and runtime

--- program/src/lib.rs
+++ program/src/lib.rs
@@ -14,12 +14,3 @@ pub mod state;
 // See https://github.com/solana-labs/solana/pull/35233
 // solana_program::declare_id!("AddressLookupTab1e1111111111111111111111111");
 solana_program::declare_id!("AaoNx79M6YE3DcXfrRN4nmBcQvQPqdpowi6uEESuJdnm");
-
-/// The definition of address lookup table accounts.
-///
-/// As used by the `crate::message::v0` message format.
-#[derive(Debug, PartialEq, Eq, Clone)]
-pub struct AddressLookupTableAccount {
-    pub key: solana_program::pubkey::Pubkey,
-    pub addresses: Vec<solana_program::pubkey::Pubkey>,
-}
