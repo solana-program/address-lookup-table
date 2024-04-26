e9a30753ceec519ceb4c50e86d3940615a831cda
Loris Leiva <loris.leiva@gmail.com>
2024-04-26 11:17:34 +0100

    Use Kinobi IDL directly

--- scripts/generate-clients.mjs
+++ scripts/generate-clients.mjs
@@ -4,76 +4,8 @@ import * as k from "@metaplex-foundation/kinobi";
 import { workingDirectory } from "./utils.mjs";
 
 // Instanciate Kinobi.
-const kinobi = k.createFromIdl(
+const kinobi = k.createFromRoot(
   require(path.join(workingDirectory, "program", "idl.json"))
-);
-
-// Update programs.
-kinobi.update(
-  k.updateProgramsVisitor({
-    solanaProgramAddressLookupTable: { name: "addressLookupTable" },
-  })
-);
-
-// Update accounts.
-kinobi.update(
-  k.updateAccountsVisitor({
-    addressLookupTable: {
-      seeds: [
-        k.variablePdaSeedNode(
-          "authority",
-          k.publicKeyTypeNode(),
-          "The address of the LUT's authority"
-        ),
-        k.variablePdaSeedNode(
-          "recentSlot",
-          k.numberTypeNode("u64"),
-          "The recent slot associated with the LUT"
-        ),
-      ],
-    },
-  })
-);
-
-// Update instructions.
-kinobi.update(
-  k.updateInstructionsVisitor({
-    createLookupTable: {
-      byteDeltas: [k.instructionByteDeltaNode(k.numberValueNode(56))],
-      accounts: {
-        address: { defaultValue: k.pdaValueNode("addressLookupTable") },
-        payer: { defaultValue: k.accountValueNode("authority") },
-      },
-      arguments: {
-        bump: { defaultValue: k.accountBumpValueNode("address") },
-      },
-    },
-    extendLookupTable: {
-      byteDeltas: [
-        k.instructionByteDeltaNode(
-          k.resolverValueNode("resolveExtendLookupTableBytes", {
-            dependsOn: [k.argumentValueNode("addresses")],
-          })
-        ),
-      ],
-    },
-  })
-);
-
-// Set account discriminators.
-kinobi.update(
-  k.setAccountDiscriminatorFromFieldVisitor({
-    addressLookupTable: { field: "discriminator", value: k.numberValueNode(1) },
-  })
-);
-
-// Set default values for structs.
-kinobi.update(
-  k.setStructDefaultValuesVisitor({
-    addressLookupTable: {
-      padding: { value: k.numberValueNode(0), strategy: "omitted" },
-    },
-  })
 );
 
 // Render JavaScript.
