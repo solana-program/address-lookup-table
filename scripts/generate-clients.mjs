5a0610ea11a3e05228ec9518047dd5ca66193550
Loris Leiva <loris.leiva@gmail.com>
2024-05-08 09:17:14 +0100

    Update Kinobi and pnpm

--- scripts/generate-clients.mjs
+++ scripts/generate-clients.mjs
@@ -1,26 +1,27 @@
 #!/usr/bin/env zx
 import "zx/globals";
-import * as k from "@metaplex-foundation/kinobi";
+import { createFromRoot } from "kinobi";
+import { renderVisitor as renderJavaScriptVisitor } from "@kinobi-so/renderers-js";
+import { renderVisitor as renderRustVisitor } from "@kinobi-so/renderers-rust";
 import { workingDirectory } from "./utils.mjs";
 
 // Instanciate Kinobi.
-const kinobi = k.createFromRoot(
+const kinobi = createFromRoot(
   require(path.join(workingDirectory, "program", "idl.json"))
 );
 
 // Render JavaScript.
 const jsClient = path.join(__dirname, "..", "clients", "js");
 kinobi.accept(
-  k.renderJavaScriptExperimentalVisitor(
-    path.join(jsClient, "src", "generated"),
-    { prettier: require(path.join(jsClient, ".prettierrc.json")) }
-  )
+  renderJavaScriptVisitor(path.join(jsClient, "src", "generated"), {
+    prettier: require(path.join(jsClient, ".prettierrc.json")),
+  })
 );
 
 // Render Rust.
 const rustClient = path.join(__dirname, "..", "clients", "rust");
 kinobi.accept(
-  k.renderRustVisitor(path.join(rustClient, "src", "generated"), {
+  renderRustVisitor(path.join(rustClient, "src", "generated"), {
     formatCode: true,
     crateFolder: rustClient,
   })
