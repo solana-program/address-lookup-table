305ebc3f03d48ce847944122cd7a8f0051e625b6
Loris Leiva <loris.leiva@gmail.com>
2024-07-29 14:14:57 +0100

    Update CI

--- scripts/generate-clients.mjs
+++ scripts/generate-clients.mjs
@@ -1,27 +1,27 @@
 #!/usr/bin/env zx
-import "zx/globals";
-import { createFromRoot } from "kinobi";
-import { renderVisitor as renderJavaScriptVisitor } from "@kinobi-so/renderers-js";
-import { renderVisitor as renderRustVisitor } from "@kinobi-so/renderers-rust";
-import { workingDirectory } from "./utils.mjs";
+import 'zx/globals';
+import { createFromRoot } from 'kinobi';
+import { renderVisitor as renderJavaScriptVisitor } from '@kinobi-so/renderers-js';
+import { renderVisitor as renderRustVisitor } from '@kinobi-so/renderers-rust';
+import { workingDirectory } from './utils.mjs';
 
 // Instanciate Kinobi.
 const kinobi = createFromRoot(
-  require(path.join(workingDirectory, "program", "idl.json"))
+  require(path.join(workingDirectory, 'program', 'idl.json'))
 );
 
 // Render JavaScript.
-const jsClient = path.join(__dirname, "..", "clients", "js");
+const jsClient = path.join(__dirname, '..', 'clients', 'js');
 kinobi.accept(
-  renderJavaScriptVisitor(path.join(jsClient, "src", "generated"), {
-    prettier: require(path.join(jsClient, ".prettierrc.json")),
+  renderJavaScriptVisitor(path.join(jsClient, 'src', 'generated'), {
+    prettier: require(path.join(jsClient, '.prettierrc.json')),
   })
 );
 
 // Render Rust.
-const rustClient = path.join(__dirname, "..", "clients", "rust");
+const rustClient = path.join(__dirname, '..', 'clients', 'rust');
 kinobi.accept(
-  renderRustVisitor(path.join(rustClient, "src", "generated"), {
+  renderRustVisitor(path.join(rustClient, 'src', 'generated'), {
     formatCode: true,
     crateFolder: rustClient,
   })
