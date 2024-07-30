cc3275dd389d982cd5a9055be4d607d715f44aa9
Loris Leiva <loris.leiva@gmail.com>
2024-07-30 11:39:49 +0100

    wip

--- scripts/program/build.mjs
+++ scripts/program/build.mjs
@@ -9,9 +9,8 @@ import {
 // Save external programs binaries to the output directory.
 import './dump.mjs';
 
-// Configure additional arguments here, e.g.:
-// ['--arg1', '--arg2', ...cliArguments()]
-const buildArgs = cliArguments();
+// Configure arguments here.
+const buildArgs = ['--features', 'bpf-entrypoint', ...cliArguments()];
 
 // Build the programs.
 await Promise.all(
