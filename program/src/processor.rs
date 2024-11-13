654736b4416f76dc19226d1db23c4b2341b45f91
Joe C <joecaulfield29@yahoo.com>
2024-11-14 00:59:31 +0800

    program: reorder lamport deduction on close (#46)

--- program/src/processor.rs
+++ program/src/processor.rs
@@ -533,11 +533,10 @@ fn process_close_lookup_table(program_id: &Pubkey, accounts: &[AccountInfo]) -> 
         .lamports()
         .checked_add(recipient_info.lamports())
         .ok_or::<ProgramError>(ProgramError::ArithmeticOverflow)?;
-
-    **lookup_table_info.try_borrow_mut_lamports()? = 0;
     **recipient_info.try_borrow_mut_lamports()? = new_recipient_lamports;
 
     // Lookup tables are _not_ reassigned when closed.
+    **lookup_table_info.try_borrow_mut_lamports()? = 0;
     lookup_table_info.realloc(0, true)?;
 
     Ok(())
