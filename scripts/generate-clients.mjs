b1fc54bf296edda8fe8fe97fa14c086f617246be
Jon C <me@jonc.dev>
2025-09-23 18:37:33 +0200

    Add rust back to codama setup

--- scripts/generate-clients.mjs
+++ scripts/generate-clients.mjs
@@ -2,9 +2,33 @@
 import 'zx/globals';
 import { createFromRoot } from 'codama';
 import { renderVisitor as renderJavaScriptVisitor } from '@codama/renderers-js';
-// import { renderVisitor as renderRustVisitor } from "@codama/renderers-rust";
+import { renderVisitor as renderRustVisitor } from '@codama/renderers-rust';
+import { parse as parseToml } from '@iarna/toml';
 
 const workingDirectory = (await $`pwd`.quiet()).toString().trim();
+
+function getCargo(folder) {
+  return parseToml(
+    fs.readFileSync(
+      path.join(workingDirectory, folder ? folder : '.', 'Cargo.toml'),
+      'utf8'
+    )
+  );
+}
+
+function getCargoMetadata(folder) {
+  const cargo = getCargo(folder);
+  return folder ? cargo?.package?.metadata : cargo?.workspace?.metadata;
+}
+
+function getToolchain(operation) {
+  return getCargoMetadata()?.toolchains?.[operation];
+}
+
+function getToolchainArgument(operation) {
+  const channel = getToolchain(operation);
+  return channel ? `+${channel}` : '';
+}
 
 // Instanciate Codama.
 const codama = createFromRoot(
@@ -20,10 +44,12 @@ codama.accept(
 );
 
 // Render Rust.
-// const rustClient = path.join(__dirname, "..", "clients", "rust");
-// codama.accept(
-//   renderRustVisitor(path.join(rustClient, "src", "generated"), {
-//     formatCode: true,
-//     crateFolder: rustClient,
-//   })
-// );
+const rustClient = path.join(__dirname, '..', 'clients', 'rust');
+codama.accept(
+  renderRustVisitor(path.join(rustClient, 'src', 'generated'), {
+    anchorTraits: false,
+    formatCode: true,
+    crateFolder: rustClient,
+    toolchain: getToolchainArgument('format'),
+  })
+);
